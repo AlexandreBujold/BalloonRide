530c0057cf3216559cce5aa9119aa3e366d00392
Loims <34040415+Loims@users.noreply.github.com>
2020-03-11 18:35:43 -0400

    Add modular gust sources
    - Gust sources inherit from GustSource.cs

---
+++ Assets/_GAME/Scripts/Hazards/GustSource.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GustSource : MonoBehaviour
+{
+    protected enum Direction
+    {
+        NULL,
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN
+    }
+
+    [Header("Wing Gust Direction")]
+    [SerializeField] protected Direction m_facing;
+
+    protected delegate void GustDel();
+
+    protected GustDel gustDelegate;
+
+    protected virtual void OnEnable()
+    {
+        InitializeDirection(m_facing);
+        gustDelegate = GustConsistent;
+    }
+
+    protected virtual void Update()
+    {
+        gustDelegate();
+    }
+
+    protected void GustConsistent()
+    {
+
+    }
+
+    protected void InitializeDirection(Direction dir)
+    {
+        if (m_facing == Direction.NULL)
+        {
+            m_facing = Direction.RIGHT;
+        }
+    }
+}
