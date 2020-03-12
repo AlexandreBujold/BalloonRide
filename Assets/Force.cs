33d7431c1a81b4ee7cb1975b072b72d4a0f1060d
Alexandre Bujold <c.alexandre.bujold@gmail.com>
2020-03-12 14:33:08 -0400

    Messing with Hinge

---
+++ Assets/Force.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour
+{
+    public float x = 5f;
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.AddForce(Vector2.right * x * Input.GetAxis("Horizontal"));
+    }
+}
