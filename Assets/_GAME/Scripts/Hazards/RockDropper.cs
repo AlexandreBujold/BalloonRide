47fc712f72234c48394f1a444bb151c4ad0a2a1a
Loims <34040415+Loims@users.noreply.github.com>
2020-03-23 15:31:10 -0400

    Add rock dropper
    - Rock dropper drops rock objects on a timer.
    - Rocks disappear on collision or after a timer

---
+++ Assets/_GAME/Scripts/Hazards/RockDropper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BalloonGame.RockDropper
+{
+    public class RockDropper : MonoBehaviour
+    {
+        [Header("Gust Coroutine")]
+        [SerializeField] private IEnumerator rockCoroutine;
+
+        [Space]
+        [Header("Rock Drop Object")]
+        [SerializeField] private GameObject rock;
+
+        [Space]
+        [Header("Rock Drop Delay")]
+        [SerializeField] private float delay = 3f;
+
+        private bool rockDropStarted = false;
+
+        private void OnEnable()
+        {
+            rock = Resources.Load<GameObject>("Rock");
+            rockCoroutine = DropRockCoroutine(delay);
+            if(!rockDropStarted)
+            {
+                StartCoroutine(rockCoroutine);
+                rockDropStarted = true;
+            }
+        }
+
+        private IEnumerator DropRockCoroutine(float waitTime)
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(waitTime);
+                DropRock();
+                Debug.Log("Drop rock");
+            }
+        }
+
+        private void DropRock()
+        {
+            Instantiate(rock, transform.position, Quaternion.identity);
+        }
+    }
+}
