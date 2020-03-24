96495d4ade808b9b7492016d72119374761de623
Alexandre Bujold <c.alexandre.bujold@outlook.com>
2020-03-23 22:16:32 -0400

    Add Balloon Fuel & Pickups
    - Add TextMeshPro
    - Add BalloonFuel and its corresponding UI
    - Add Pickup functionality
    - Add CameraFollow.cs

---
+++ Assets/_GAME/Scripts/Balloon/BalloonFuelUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BalloonGame.Balloon.Fuel;
+
+public class BalloonFuelUI : MonoBehaviour
+{
+    public Slider fuelSlider;
+    public TMPro.TextMeshProUGUI fuelText;
+    public BalloonFuel m_balloonFuel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (null != fuelSlider)
+        //{
+        //    if (null != m_balloonFuel)
+        //    {
+        //        fuelSlider.maxValue = m_balloonFuel.GetMaxFuel();
+        //    }
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null != fuelSlider)
+        {
+            if (null != m_balloonFuel)
+            {
+                fuelSlider.value = m_balloonFuel.GetFuel() / m_balloonFuel.GetMaxFuel();
+
+                if (fuelText != null)
+                {
+                    fuelText.SetText(m_balloonFuel.GetFuel().ToString("F1"));
+                }
+            }
+        }
+    }
+}
