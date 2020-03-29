fd77f826c62d54531c367bc3e9b46aa04732f268
Loims <34040415+Loims@users.noreply.github.com>
2020-03-28 20:04:22 -0400

    Add pause menu
    - Add pause menu functionality
    - Change scenes to accommodate

---
+++ Assets/_GAME/Scripts/Menu/DisableCanvasDespawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCanvasDespawn : MonoBehaviour
+{
+    private bool instanceInit = false;
+
+    private void OnEnable()
+    {
+        if (!instanceInit)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instanceInit = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
